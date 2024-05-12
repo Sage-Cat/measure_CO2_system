1813c127ee4fe4676dddcb579aa2dff0e42eb1a8
Sage-Cat <sagecatbox@gmail.com>
2024-05-12 23:10:52 +0300

    Implement base application layer. Get after feature
    Signed-off-by: Sage-Cat <sagecatbox@gmail.com>

--- server/src/Application/Application.cpp
+++ server/src/Application/Application.cpp
@@ -1,13 +1,29 @@
 #include "Application.hpp"
 
-Application::Application(CO2Sensor &co2Sensor, SQLiteDatabase &sqliteDatabase)
-    : sensor(co2Sensor), database(sqliteDatabase)
+#include "Database/SQLiteDatabase.hpp"
+
+#include "SpdlogConfig.hpp"
+
+Application::Application(CO2Sensor &co2Sensor, SQLiteDatabase &db)
+    : sensor_(co2Sensor), db_(db)
 {
+    SPDLOG_TRACE("Application::Application");
 }
 
 void Application::doTask(RequestData data, SendResponseCallback callback)
 {
+    SPDLOG_TRACE("Application::doTask | cmd - {}", data.cmd);
     ResponseData resData{};
+
+    if (data.cmd == "get_all") 
+    { 
+        // geting all records using the most early date
+        resData.measurements = db_.getMeasurementsAfterDate("");
+    } else if (data.cmd == "get_after") {
+        resData.measurements = db_.getMeasurementsAfterDate(data.param1);
+    } else {
+        SPDLOG_WARN("Application::doTask | unknown cmd");
+    }
 
     callback(resData);
 }
--- server/src/Application/Application.hpp
+++ server/src/Application/Application.hpp
@@ -1,23 +1,23 @@
 #ifndef APPLICATION_HPP
 #define APPLICATION_HPP
 
-#include "Data.hpp"
 #include <functional>
-#include <sqlite3.h>
+
+#include "Data.hpp"
 
 class CO2Sensor;
 class SQLiteDatabase;
 
 class Application {
 public:
-    Application(CO2Sensor &co2Sensor, SQLiteDatabase &sqliteDatabase);
+    Application(CO2Sensor &co2Sensor, SQLiteDatabase &db);
     ~Application() = default;
 
     void doTask(RequestData data, SendResponseCallback callback);
 
 private:
-    CO2Sensor &sensor;
-    SQLiteDatabase &database;
+    CO2Sensor &sensor_;
+    SQLiteDatabase &db_;
 };
 
 #endif
