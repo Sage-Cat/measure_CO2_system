a11eaf5416d2fa2a10f6369b32c5750af68e4c65
IhorBerehovskyi <ihor.berehovskyi.ki.2020@lpnu.ua>
2024-05-15 21:15:22 +0300

    Implemented base fuctional

--- client/settings.gradle.kts
+++ client/settings.gradle.kts
@@ -14,10 +14,8 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-
         google()
         mavenCentral()
-        maven { url = uri("https://www.jitpack.io") }
     }
 }
 
