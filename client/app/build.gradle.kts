16f6c6217117c0746c96cf9571b7ed7d6e50a729
IhorBerehovskyi <ihor.berehovskyi.ki.2020@lpnu.ua>
2024-05-16 03:51:16 +0300

    Implement client menu and basic chart

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
